99b5c14d5e783ff634409c7855cb01bc666c722a
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-11-10 16:28:02 +0000

    base structure for api

---
+++ src/api.js
@@ -0,0 +1,46 @@
+let cityData;
+let cityAQI;
+const getData = async (input) => {
+  let city = input;
+  let lat;
+  let lon;
+
+  const getCoord = async () => {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=241019ef2c8c83b71acba7e390ce2fac`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    lat = data[0].lat;
+    lon = data[0].lon;
+    return { lat, lon };
+  };
+
+  const getCityData = async () => {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=4&appid=241019ef2c8c83b71acba7e390ce2fac&units=metric`,
+      { mode: "cors" }
+    );
+    return (cityData = await response.json());
+  };
+
+  const getCityAQI = async () => {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=241019ef2c8c83b71acba7e390ce2fac`,
+      { mode: "cors" }
+    );
+    return (cityAQI = await response.json());
+  };
+
+  const getAllData = async () => {
+    await getCityData();
+    await getCityAQI();
+  };
+
+  await getCoord();
+
+  await getAllData();
+  console.log(cityAQI);
+};
+console.log(cityAQI);
+export { getData };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("webpack connected");
+import { getData} from "./api";
+
+
+// default
+getData("London");
+
+
+const searchBtn = document.querySelector("#searchBtn");
+
+const newInput = async () => {
+  let input = document.querySelector("#input").value;
+  await getData(input);
+ 
+  input = "";
+ 
+};
+
+searchBtn.addEventListener("click", newInput);
